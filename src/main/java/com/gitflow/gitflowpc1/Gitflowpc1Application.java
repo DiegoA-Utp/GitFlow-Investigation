a04e8a5dec6549d42f1988b124ec17a899e303eb
DiegoA-Utp <u20218230@utp.edu.pe>
2025-06-11 15:03:01 -0500

    first commit

---
+++ src/main/java/com/gitflow/gitflowpc1/Gitflowpc1Application.java
@@ -0,0 +1,13 @@
+package com.gitflow.gitflowpc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gitflowpc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gitflowpc1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitflow/gitflowpc1/Gitflowpc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitflow.gitflowpc1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gitflowpc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
